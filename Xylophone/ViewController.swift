de6c561e6d65778aa46db9d82538c822ff83ac98
Felipe Augusto <augusto_felipe@icloud.com>
2022-07-14 20:54:43 -0300

    First Commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //let buttonTitle = sender.currentTitle!
+        sender.alpha = 0.5
+        print("Start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1.0
+        }
+        print("End")
+        
+        playSound(soudName: sender.currentTitle!)
+    }
+    
+    func playSound(soudName: String) {
+        let url = Bundle.main.url(forResource: soudName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
     
 
 }
